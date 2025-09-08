1ffaad033378d3d9f47c6c010d03b451fc730c92
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-09-08 18:24:58 +0800

    injection helper

---
+++ Loto3000App/Lotto3000App/Lotto3000App.Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,16 @@
+﻿using Lotto3000App.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Lotto3000App.Helpers
+
+{
+    public static class DependencyInjectionHelper
+    {
+        public static void InjectDbContext(IServiceCollection services)
+            {
+                services.AddDbContext<Lotto3000DbContext>(x => x.UseSqlServer("Server=.\\SQLExpress;Database=Lotto3000App;Trusted_Connection=True;TrustServerCertificate=True"));
+            }
+    }
+}
