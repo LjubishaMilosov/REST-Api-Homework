fb0a3d0995e163bb6d4d8ed77bd633d62fe3f827
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-10-15 02:46:18 +0800

    moviesApp mapper

---
+++ MoviesApp/MoviesApp/MoviesApp.Mappers/MovieMapper.cs
@@ -0,0 +1,32 @@
+﻿using MoviesApp.Domaim.Models;
+using MoviesApp.Dtos;
+
+namespace MoviesApp.Mappers
+{
+    public static class MovieMapper
+    {
+        // from domain class to movieDto
+        public static MovieDto ToMovieDto(this Movie movie) // extention method
+        {
+            return new MovieDto
+            {
+                Title = movie.Title,
+                Description = movie.Description,
+                Year = movie.Year,
+                Genre = movie.Genre
+            };
+        }
+
+        // from dto to domain class
+        public static Movie ToMovie(this AddMovieDto addMovie)
+        {
+            return new Movie
+            {
+                Title = addMovie.Title,
+                Description = addMovie.Description,
+                Year = addMovie.Year,
+                Genre = addMovie.Genre
+            };
+        }
+    }
+}
